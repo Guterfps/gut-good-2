14860fbcba564a33a20f7700e402ca600885c610
Guterfps <51312180+Guterfps@users.noreply.github.com>
2019-06-22 10:40:39 +0300

    twit mathfucker

--- main.js
+++ main.js
@@ -7,10 +7,32 @@ const post=()=>{
     tweeter.addPost($("#input").val())
     renderer.renderPosts(tweeter.getPosts())
 }
-$(".delete").click(function(){
-   const id=$(this).closest(".post").attr("data-id")
+
+$("body").on("click",".delete",function(){
+   let id=$(this).closest(".post").attr("data-id")
     tweeter.removePost(id)
     console.log(id)
     renderer.renderPosts(tweeter.getPosts())
+
 })
+
+
+$("body").on("click",".comment",function(){
+   let id=$(this).closest(".post").attr("data-id")
+   let text=$(this).siblings("input.comments").val()
+    tweeter.addComment(id,text)
+    renderer.renderPosts(tweeter.getPosts())
+    })
+
+
+
+    $("body").on("click",".delete-comment",function(){
+    let ID=$(this).closest(".post").attr("data-id")
+   let id=$(this).closest("div.comments").attr("data-id")
+    tweeter.removeComment(ID,id)
+    renderer.renderPosts(tweeter.getPosts())
+    })
+
+
+
 
--- render.js
+++ render.js
@@ -2,16 +2,14 @@ const Renderer=function(){
     const renderPosts=function(posts){
 $("#posts").empty()
 for ( let i=0;i< posts.length;i++){
-   
     $("#posts").append("<div class='post' data-id="+posts[i].id+" >"+posts[i].text+"</div>")
-    $("[data-id="+posts[i].id+"]").append("<button  class='delete'></button>")
+    $("[data-id="+posts[i].id+"]").append("<button  class='delete' ></button>")
 for(let x=0;x<posts[i].comments.length;x++){
-   $("[data-id="+posts[i].id+"]").append("<button  class='delete-comment'></button>")
     $("[data-id="+posts[i].id+"]").append("<div class='comments' data-id="+posts[i].comments[x].id+">"+posts[i].comments[x].text+"</div>")
-    
+    $("[data-id="+posts[i].comments[x].id+"]").append("<button  class='delete-comment' ></button>")
 }
 $("[data-id="+posts[i].id+"]").append("<input class='comments'>")
-$("[data-id="+posts[i].id+"]").append("<button  class='comments'>comment</button>")
+$("[data-id="+posts[i].id+"]").append("<button class='comment' >comment</button>")
 }
     }
     
